5e8b182f26011a79c22ff5f7a41e5eb00621cbd4
Thyago Santos <thyagomatheus15@gmail.com>
2023-03-09 23:13:26 -0300

    Update gen-rss.js

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -5,9 +5,9 @@ const matter = require('gray-matter')
 
 async function generate() {
   const feed = new RSS({
-    title: 'Your Name',
-    site_url: 'https://yoursite.com',
-    feed_url: 'https://yoursite.com/feed.xml'
+    title: 'Thyago Santos',
+    site_url: '#',
+    feed_url: '#'
   })
 
   const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
