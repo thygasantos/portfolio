962abbdd65929a6500ff43e254c83402b06f0823
Thyago Santos <thyagomatheus15@gmail.com>
2023-04-24 12:24:14 -0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import 'nextra-theme-blog/style.css'
 import Head from 'next/head'
 
 import '../styles/main.css'
-import '../styles/style.css'
+
 
 export default function Nextra({ Component, pageProps }) {
   return (
